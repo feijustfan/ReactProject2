222ce6c7debed9ebdf7f75ec071a20d15b3491f8
feifan <ffhuang07@gmail.com>
2021-02-11 18:53:09 +1100

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+function parseBetterJSAlternative(code) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+  syntaxTree.forEach(node => {
+    // parse...
+  });
+}
+
+function tokenize(code) {
+  const REGEXES = [
+    // ...
+  ];
+
+  const statements = code.split(" ");
+  const tokens = [];
+  REGEXES.forEach(REGEX => {
+    statements.forEach(statement => {
+      tokens.push(/* ... */);
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens) {
+  const syntaxTree = [];
+  tokens.forEach(token => {
+    syntaxTree.push(/* ... */);
+  });
+
+  return syntaxTree;
+}
+
