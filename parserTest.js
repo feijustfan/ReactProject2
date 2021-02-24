7cacb25c3cffa9514a7aca8547b87544f391d833
feifan <fifnhuang@gmail.com>
2021-02-24 22:44:50 +1100

    binary expression function

--- parserTest.js
+++ parserTest.js
@@ -1,8 +1,26 @@
 //set operator precedence
 var PRECEDENCE = {
-  "+": 1,
-  "-": 1,
-  "*": 2,
-  "/": 2,
+  "=": 1,
+  "+": 2,
+  "-": 2,
+  "*": 3,
+  "/": 3,
 }
 
+//
+function maybe_binary(left, my_prec){
+  //tk for token
+  var tk = is_op();
+  if (tk){
+    var his_prec = PRECEDENCE[tk.value];
+    if(his_prec > my_prec){
+      Input.next();
+      return maybe_binary({
+        type: tk.value == "="? "assign":"binary",
+        operator: tk.value,
+        lef: left,
+        right: maybe_binary(parse_atom(), his_prec)
+      },my_prec)
+    }
+  }
+}
