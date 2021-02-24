2671c3007c7e7a4e053db2306dcd8a7a96b1dab5
feifan <fifnhuang@gmail.com>
2021-02-24 23:48:10 +1100

    tokenizer

---
+++ tokenizer.js
@@ -0,0 +1,18 @@
+//tokenizer
+function tokenizer(input){
+  var tk = [];
+
+  function croakErr(msg){
+    throw new console.error(msg);
+    
+  }
+}
+//process 1 digit, 2 digit, 3 digit numbers... 
+function numMorethan9(strnum){
+  var numOutput = strnum[0];
+  for(let i = 1; strnum[i];i++){
+    numOutput = numOutput*10 + strnum[i];
+    
+
+  }
+}
